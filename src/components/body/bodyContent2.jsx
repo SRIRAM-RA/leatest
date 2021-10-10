ed2c99bdb5c3e154f49a1bed4508c227c4f5eb05
Gohanray <55801381+Gohanray@users.noreply.github.com>
2021-10-10 12:11:35 +0530

    reverted the changes

--- src/components/body/bodyContent2.jsx
+++ src/components/body/bodyContent2.jsx
@@ -19,29 +19,27 @@ class LearningDock extends Component{
     render(){
         const { animation, duration, visible } = this.state
         return(
-           <section className="About-box-2">
-                <div data-aos="flip-left" className="animated-box">
-                    <box className="box-2-content">
-                        <h3 className="learning-dock-content">Resources that act as tools to enhance learning and supplement classroom teaching. The online learning resources forms the idea that there exists a class of readers who respond better to online content and prefer to learn new skills at their own pace from the comforts of their drawing rooms. This website helps make the process of learning, testing and helps users to gain knowledge.</h3>
-                    </box>
-                    <box className="box-2-logo">
-                        {/* {(enter)?<div></div>:<div></div>} */}
-                        <Header as='h2' icon>
-                            <div>
-                                <i class="fas fa-atlas landing-logo"></i>
-                            </div>
-                            <Transition
-                                animation={animation}
-                                duration={duration}
-                                visible={visible}
-                                >
-                                    <h2 className="learning-head" onMouseUp={this.toggleVisibility}>Learning Dock</h2>
-                            </Transition>
-                        </Header>
-                        <br />
-                        <Button1 />
-                    </box>
-                </div>
+           <section className="About-box-2" data-aos="flip-left" >
+               <box className="box-2-content">
+                   <h3 className="learning-dock-content">Resources that act as tools to enhance learning and supplement classroom teaching. The online learning resources forms the idea that there exists a class of readers who respond better to online content and prefer to learn new skills at their own pace from the comforts of their drawing rooms. This website helps make the process of learning, testing and helps users to gain knowledge.</h3>
+               </box>
+               <box className="box-2-logo">
+                   {/* {(enter)?<div></div>:<div></div>} */}
+               <Header as='h2' icon>
+                    <div>
+                        <i class="fas fa-atlas landing-logo"></i>
+                    </div>
+                    <Transition
+                        animation={animation}
+                        duration={duration}
+                        visible={visible}
+                        >
+                            <h2 className="learning-head" onMouseUp={this.toggleVisibility}>Learning Dock</h2>
+                    </Transition>
+                </Header>
+                <br />
+                <Button1 />
+               </box>
            </section>
         )
     }
--- src/components/body/bodyContent4.jsx
+++ src/components/body/bodyContent4.jsx
@@ -18,77 +18,75 @@ class FooterComponent extends Component {
 
         const { animation, duration, visible } = this.state
         return(
-            <section className="footer" >
-                <div data-aos="fade-up" className="animated-box">
-                    <box className="footer-box">
-                        <box className="footer-outer-box-1">
-                            <box className="footer-box-1">
-                                <Transition
-                                animation={animation}
-                                duration={duration}
-                                visible={visible}
-                                >
-                                    <h3 id="follow" onMouseUp={this.toggleVisibility}>Follow us</h3>
-                                </Transition>
-                                <div className="follow-icons">
-                                    <a href="#"><Icon name='facebook f' /></a>
-                                    <a href="#"><Icon name='twitter' /></a>
-                                    <a href="#"><Icon name='google plus g' /></a>
-                                    <a href="#"><Icon name='linkedin alternate' /></a>
-                                    <a href="#"><Icon name='instagram' /></a>
-                                </div>
-                            </box>
-                            <box className="partner">
-                                <img src="https://spotknack.com/mailimages/spotknacklogo.png" className="spotknack-logo" />
-                            </box>
+            <section className="footer" data-aos="fade-up" >
+                <box className="footer-box">
+                    <box className="footer-outer-box-1">
+                        <box className="footer-box-1">
+                            <Transition
+                            animation={animation}
+                            duration={duration}
+                            visible={visible}
+                            >
+                                <h3 id="follow" onMouseUp={this.toggleVisibility}>Follow us</h3>
+                            </Transition>
+                            <div className="follow-icons">
+                                <a href="#"><Icon name='facebook f' /></a>
+                                <a href="#"><Icon name='twitter' /></a>
+                                <a href="#"><Icon name='google plus g' /></a>
+                                <a href="#"><Icon name='linkedin alternate' /></a>
+                                <a href="#"><Icon name='instagram' /></a>
+                            </div>
                         </box>
-                        <box className="footer-outer-box-2">
-                            <box className="footer-inner-box-1">
-                                <Icon name="map" size="large"/>
-                                <box className="location-text">
-                                    <h3>Tamil Nadu, India</h3>
-                                    <h5>Coimbatore</h5>
-                                </box>
-                            </box>
-                            <box className="footer-inner-box-2">
-                                <h2 id="links-head">Useful Links</h2>
-                                <div className="links-box">
-                                    <a href="#"> About Us</a>
-                                    <a href="#"> Services</a>
-                                    <a href="#"> Projects</a>
-                                    <a href="#"> Our Team</a>
-                                    <a href="#"> Contact Us</a>
-                                </div>
-                            </box>
-                        </box>
-                        <box className="footer-outer-box-3">
-                            <box className="footer-inner-box-1">
-                                <Icon name="phone volume" size="large"/> 
-                                <box className="location-text">
-                                    <h3>+91 9876543201</h3>
-                                    <h5>Give us a call</h5>
-                                </box>
-                            </box>
-                            <box className="footer-inner-box-2">
-                                <h2 id="links-head">Useful Links</h2>
-                                <div id="moto">
-                                    <Icon name="quote left" />
-                                    <p>A Great place for Education</p>
-                                </div>
-                                <Input
-                                icon={{ name: 'search', circular: true, link: true }}
-                                placeholder='Search...'
-                                required
-                                inverted
-                                className="email-footer"
-                                />
-                            </box>
+                        <box className="partner">
+                            <img src="https://spotknack.com/mailimages/spotknacklogo.png" className="spotknack-logo" />
                         </box>
                     </box>
-                    <box className="footer-box1">
-                        <h3 id="copyrights">Copyright © 2021, All Right Reserved | SRIRAM</h3>
+                    <box className="footer-outer-box-2">
+                        <box className="footer-inner-box-1">
+                            <Icon name="map" size="large"/>
+                            <box className="location-text">
+                                <h3>Tamil Nadu, India</h3>
+                                <h5>Coimbatore</h5>
+                            </box>
+                        </box>
+                        <box className="footer-inner-box-2">
+                            <h2 id="links-head">Useful Links</h2>
+                            <div className="links-box">
+                                <a href="#"> About Us</a>
+                                <a href="#"> Services</a>
+                                <a href="#"> Projects</a>
+                                <a href="#"> Our Team</a>
+                                <a href="#"> Contact Us</a>
+                            </div>
+                        </box>
                     </box>
-                </div>
+                    <box className="footer-outer-box-3">
+                        <box className="footer-inner-box-1">
+                            <Icon name="phone volume" size="large"/> 
+                            <box className="location-text">
+                                <h3>+91 9876543201</h3>
+                                <h5>Give us a call</h5>
+                            </box>
+                        </box>
+                        <box className="footer-inner-box-2">
+                            <h2 id="links-head">Useful Links</h2>
+                            <div id="moto">
+                                <Icon name="quote left" />
+                                <p>A Great place for Education</p>
+                            </div>
+                            <Input
+                            icon={{ name: 'search', circular: true, link: true }}
+                            placeholder='Search...'
+                            required
+                            inverted
+                            className="email-footer"
+                            />
+                        </box>
+                    </box>
+                </box>
+                <box className="footer-box1">
+                    <h3 id="copyrights">Copyright © 2021, All Right Reserved | SRIRAM</h3>
+                </box>
             </section>
         )
     }
